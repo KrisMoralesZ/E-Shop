b94857e6455835982ef435a1cb982ea6b22871f6
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2023-07-22 10:01:56 -0700

    Create Card Component

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Layout from "../../Components/Layout";
+import Card from "../../Components/Card";
 
 const Home = () => {
   return (
     <Layout>
       Home
+      <Card />
     </Layout>
   )
 }
