ef13663787a9eaea034bc681d680f1734ab94da5
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2023-11-22 14:59:46 -0700

    Create Order Cart

--- src/Components/CheckOutSideMenu/CheckOutSideMenu.jsx
+++ src/Components/CheckOutSideMenu/CheckOutSideMenu.jsx
@@ -1,10 +1,11 @@
 import React, { useContext } from "react";
 import { XMarkIcon } from '@heroicons/react/24/solid'
+import OrderCart from "../OrderCard";
 import { ShoppingCartContext } from "../../Context";
 import './CheckOutSideMenu.css'
 
 const CheckOutSideMenu = () => {
-  const { isCheckOutSideMenuOpen, closeCheckOutsideMenu } = useContext(ShoppingCartContext)
+  const { isCheckOutSideMenuOpen, closeCheckOutsideMenu, cartProducts } = useContext(ShoppingCartContext)
 
   return (
     <aside className={`${isCheckOutSideMenuOpen ? 'flex' : 'hidden'} product-details flex-col fixed bg-white right-0 border border-black rounded-lg`}>
@@ -12,6 +13,18 @@ const CheckOutSideMenu = () => {
         <h2 className="font-medium text-xl">My Order</h2>
         <XMarkIcon className="h-6 w-6 text-black cursor-pointer"
           onClick={() => closeCheckOutsideMenu()}></XMarkIcon>
+      </div>
+      <div className="px-6">
+        {
+          cartProducts.map(product => (
+            <OrderCart
+              key={product.id}
+              title={product.title}
+              imageUrl={product.images}
+              price={product.price}
+            />
+          ))
+        }
       </div>
     </aside>
   )
