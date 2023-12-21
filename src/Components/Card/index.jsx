9db48085acceaf63565f1bb30df67865a7e7bcd8
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2023-12-20 20:59:35 -0700

    add only one element to cart

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import { ShoppingCartContext } from "../../Context";
-import { PlusIcon } from "@heroicons/react/24/solid";
+import { CheckIcon, PlusIcon } from "@heroicons/react/24/solid";
 
 const Card = (data) => {
   const {
@@ -27,6 +27,25 @@ const Card = (data) => {
     closeProductDetails()
   }
 
+  const renderIcon = (id) => {
+    const isInCart = cartProducts.filter(product => product.id === id).length > 0
+
+    if (isInCart) {
+      return (
+        <div className="absolute top-0 right-0 justify-center items-center bg-black w-6 h-6 rounded-full m-2 p-1">
+          <CheckIcon className="text-white" />
+        </div>
+      )
+    } else {
+      return (
+        <div className="absolute top-0 right-0 justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1"
+          onClick={(event) => addProductsToCart(event, data.data)}>
+          <PlusIcon />
+        </div>
+      )
+    }
+  }
+
   return (
     <div className="bg-white cursor-pointer w-56 h-60 rounded-lg"
       onClick={() => showProduct(data.data)}>
@@ -38,10 +57,7 @@ const Card = (data) => {
           src={data.data.images[0]}
           alt={data.data.title}
         />
-        <div className="absolute top-0 right-0 justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1"
-          onClick={(event) => addProductsToCart(event, data.data)}>
-          <PlusIcon />
-        </div>
+        {renderIcon(data.data.id)}
         <p className="flex justify-between">
           <span className="text-sm font-light">{data.data.title}</span>
           <span className="text-lg font-medium">${data.data.price}</span>
