22b22b9657c89d0738c4ea7be2ee003def431437
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2024-04-22 09:48:48 -0700

    Dynamic Checkout based on LoggedIn Status

--- src/Pages/SignIn/index.jsx
+++ src/Pages/SignIn/index.jsx
@@ -1,12 +1,13 @@
 import React, { useContext } from "react";
 import { ShoppingCartContext } from "@/Context/index";
 import { useLocalStorage } from "../../hooks/useLocalStorage";
+import LoginForm from "@/Components/LoginForm";
 
 const SignIn = () => {
   const { setLoggedIn } = useContext(ShoppingCartContext)
   const { setItem } = useLocalStorage('logged-in')
 
-  const handleSignInClick = () => {
+  const handleLogIn = () => {
     setItem('logged-in')
     setLoggedIn(true)
   }
@@ -14,12 +15,9 @@ const SignIn = () => {
   return (
     <>
       <h1>WELCOME</h1>
-      <div className="flex flex-col gap-4 justify-center">
-        <h1>Log in</h1>
-        <input type="text" placeholder="e-mail" />
-        <input type="text" placeholder="password" />
-        <button className="border bg-black text-white" onClick={() => handleSignInClick()}>Log in</button>
-      </div>
+      <LoginForm handleLogIn={handleLogIn} />
+      <h3>Don't have an account?</h3>
+      <button>Sign in</button>
     </>
   )
 }
